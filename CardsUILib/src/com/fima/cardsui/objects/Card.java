5158b1346c1f8b2160b57f519f607bc7346c4568
Christophe Smet <christophesmet@Macintosh.local>
2014-07-09 17:58:49 +0200

    Updated for new lib

--- CardsUILib/src/com/fima/cardsui/objects/Card.java
+++ CardsUILib/src/com/fima/cardsui/objects/Card.java
@@ -94,9 +94,9 @@ public abstract class Card extends AbstractCard implements IBindable {
 
     private void bindView(View v) {
         if (v != null && v instanceof IBindableView) {
-            ((IBindableView) v).bind(this,-1);
+            ((IBindableView) v).bind(this);
         } else {
-            LogcatHelper.get().d(TAG, "Card view is null or not bindable ... FIX IT !! " + v == null ? "" : v.getClass().getSimpleName());
+            LogcatHelper.get().d(TAG, "Card view is null or not bindable ... FIX IT !! " + (v == null ? "" : v.getClass().getSimpleName()));
         }
     }
 
